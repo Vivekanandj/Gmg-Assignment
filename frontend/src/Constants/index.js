c616d93c63863e107107adfd9921151a2b4ff64e
Vivekanandj <vivekanandaj867@gmail.com>
2024-02-02 13:04:04 +0530

    vivek

--- frontend/src/Constants/index.js
+++ frontend/src/Constants/index.js
@@ -1,2 +1,2 @@
 //export const backendURL = 'http://localhost:5000'
-export const backendURL ='https://gmg-assignment-backend.vercel.app'
+export const backendURL ='https://gmg-assignment.onrender.com'
